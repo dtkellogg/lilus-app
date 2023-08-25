3ba0e2bae0337c2cbeeb44cfbcc698d74f6a19ac
Toshi <dtkellogg10@gmail.com>
2023-08-25 10:13:14 -0400

    Foundational reset for the frontend (cleared the page.tsx file in /app)

---
+++ app/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1>Header</h1>
+    </div>
+  )
+}
+
+export default Header
