55aca06116bb86da2bafb0adce1abc8e69b2b1ff
Toshi <dtkellogg10@gmail.com>
2023-08-28 19:30:30 -0400

    Nav - added home button

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import Nav from './Nav';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon, HomeIcon } from '@heroicons/react/24/solid'
 
 function Header() {
   return (
     <header className="flex items-center m-4">
+      <Link href="/" prefetch={false}>
+        <HomeIcon className="w-8 h-8 cursor-pointer" />
+      </Link>
       <Nav />
       {/* <h1 className="text-4xl">Lilu&apos;s App</h1> */}
       <MagnifyingGlassIcon className="w-8 h-8 cursor-pointer" />
